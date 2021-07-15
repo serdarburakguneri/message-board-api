69891acb542cbde3cecece58e1ce6f5c09e66836
serdarburakguneri <sburakguneri@gmail.com>
2021-07-15 16:31:45 +0300

    cors configuration with a modifiable parameter added

--- src/main/java/com/sbg/msgboard/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/sbg/msgboard/infrastructure/security/SecurityConfiguration.java
@@ -1,12 +1,18 @@
 package com.sbg.msgboard.infrastructure.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Value("${security.configuration.enabled}")
   private boolean securityEnabled;
+
+  @Value("${security.configuration.allowed-origins}")
+  private String allowedOrigins;
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
@@ -39,5 +48,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
       http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
     }
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(getAllowedOrigins());
+    configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+  private List<String> getAllowedOrigins() {
+
+    // TODO: An exception might be thrown later if allowedOrigins is empty
+
+    return Arrays.asList(allowedOrigins.split(","));
   }
 }
